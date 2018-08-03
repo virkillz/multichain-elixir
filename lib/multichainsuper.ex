7a94aa49e171142a10b432c5d1f0ffa71fe04d60
Arif <arif@get.com.mm>
2018-08-03 11:01:05 +0630

    add doc to each function

--- lib/multichainsuper.ex
+++ lib/multichainsuper.ex
@@ -10,17 +10,19 @@ defmodule Multichain.Super do
   alias Multichain.Http
 
   @doc """
-  This function will return information about the connected Multichain's node.
+  This function will return a keypair which address is imported to the node as watch only address. This is used if you want to generate address which node cannot have control of it.
 
-  Usually this is used to check the connection.
 
+  Having this kind of address, only you as private key owner can spent the asset in it. The node doesn't store your private key and once you saw it it will never appear again. So ensure you record your private key somewhere secure.
+
+  This kind of address transfer security responsibility to you instead of the node server. The security of private key is handled externally.
   """
   def create_external_address do
     # 1 create keypair
     case Http.jsonrpccall("createkeypairs", [1]) do
       {:ok, result} ->
         [keypair] = result["result"]
-         # 2. import address 
+        # 2. import address 
         case Http.jsonrpccall("importaddress", [keypair["address"], "", false]) do
           # 3. grant permission sent
           {:ok, _result2} ->
@@ -38,12 +40,24 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function will return a list of all address who have issue permission.
+
+  This address can issue any new asset.
+  """
+
   def who_can_issue() do
     case Http.jsonrpccall("listpermissions", ["issue"]) do
       {:ok, result} -> {:ok, result["result"]}
       error -> error
     end
   end
+
+  @doc """
+  This function will reissue another quantity from existing assets. 
+
+  If you want to create new type of asset, use `Multichain.api/2` and its respective parameter based on Multichain API documentation.
+  """
 
   def print_money(assetname, qty) do
     case who_can_issue() do
@@ -65,6 +79,11 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function is used to transfer asset from external address, owning the private key.
+
+  If the key is handled externally, you can transfer asset from one address to another using this method. If it is internal address, use `transfer/3` instead.
+  """
   def transfer(assetcode, from, to, qty, privkey) do
     case Http.jsonrpccall("createrawsendfrom", [from, %{to => %{assetcode => qty}}]) do
       {:ok, %{"error" => nil, "id" => nil, "result" => result}} ->
@@ -86,6 +105,11 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function is used to transfer asset from internal address.
+
+  If the address is exist on `list_internal_address/0` then we can use this function to transfer asset.
+  """
   def transfer(assetcode, from, to, qty) do
     case Http.jsonrpccall("sendassetfrom", [from, to, assetcode, qty]) do
       {:ok, result} -> {:ok, result["result"]}
@@ -93,6 +117,12 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function will return all address manage and owned by this node.
+
+  If you want to see all address watched by this node but owned by external, use `list_external_address/0`
+
+  """
   def list_internal_address() do
     case Multichain.api("getaddresses", [true]) do
       {:ok, result} ->
@@ -104,13 +134,46 @@ defmodule Multichain.Super do
     end
   end
 
-  def create_internal_address() do
-    case Http.jsonrpccall("getnewaddress", []) do
-      {:ok, result} -> result
-      error -> error
+  @doc """
+  This function will return all address watched by this node but not belong to node's wallet, which means we cannot transfer any asset unless we know the private key
+
+  If you want to see all address which can be used without private key, use `list_internal_address/0`
+
+  """
+  def list_external_address() do
+    case Multichain.api("getaddresses", [true]) do
+      {:ok, result} ->
+        hasil = result["result"] |> Enum.filter(fn x -> x["ismine"] == false end)
+        {:ok, %{"count" => length(hasil), "result" => hasil}}
+
+      other ->
+        other
     end
   end
 
+  @doc """
+  This function generate internal address which can be used without private key. This will also give send and receive permission to the address.
+
+  If you want to create address without permission you can use `Multichain.api("getnewaddress", [])`
+
+  """
+  def create_internal_address() do
+    case Http.jsonrpccall("getnewaddress", []) do
+      {:ok, result} ->
+        case grant_send_receive(result["result"]) do
+          {:ok, _} -> {:ok, result["result"]}
+          other -> other
+        end
+
+      error ->
+        error
+    end
+  end
+
+  @doc """
+  This function top up any code from primary Node's wallet. Also the address which usually have issue permission.
+
+  """
   def topup(address, assetcode, qty) do
     case Http.jsonrpccall("sendasset", [address, assetcode, qty]) do
       {:ok, result} -> {:ok, result["result"]}
@@ -118,6 +181,9 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function is used to revoke send permission of a particular address.
+  """
   def block(address) do
     case Http.jsonrpccall("revoke", [address, "send"]) do
       {:ok, result} -> {:ok, result["result"]}
@@ -125,6 +191,9 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This function is used to give send permission to address which previously has been blocked.
+  """
   def unblock(address) do
     case Http.jsonrpccall("grant", [address, "send"]) do
       {:ok, result} -> {:ok, result["result"]}
@@ -132,6 +201,9 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This is a helper to grant_send and receive to particular address.
+  """
   def grant_send_receive(address) do
     case Http.jsonrpccall("grant", [address, "send,receive"]) do
       {:ok, result} -> {:ok, result["result"]}
@@ -139,6 +211,11 @@ defmodule Multichain.Super do
     end
   end
 
+  @doc """
+  This is a helper function to check whether an address have send permission or not.
+
+  You can unblock (give send permission) by using `unblock/1`
+  """
   def blockstatus(address) do
     case Multichain.api("listpermissions", ["*", address]) do
       {:ok, result} -> find_send_permission(result["result"])
